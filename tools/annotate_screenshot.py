3086c4d32e79821a1d389d962f81002076c9c511
Lucas Magasweran <lucasrangit@gmail.com>
2026-06-05 20:20:33 +0200

    rename

--- tools/annotate_screenshot.py
+++ tools/annotate_screenshot.py
@@ -2,8 +2,8 @@ from PIL import Image, ImageDraw, ImageFont
 import os
 
 # Load the image
-img_path = 'docs/word-count-timeline-screenshot.png'
-output_path = 'docs/word-count-timeline-annotated.png'
+img_path = 'docs/screenshot.png'
+output_path = 'docs/screenshot-annotated.png'
 
 try:
     img = Image.open(img_path).convert('RGB')
@@ -29,26 +29,26 @@ except:
 for (x1, y1, x2, y2, label) in features:
     # Draw rectangle
     draw.rectangle([x1, y1, x2, y2], outline="red", width=5)
-    
+
     # Calculate vertical center of the box
     y_center = (y1 + y2) // 2
-    
+
     # Calculate text position (to the left of the box)
     text_bbox = draw.textbbox((0, 0), label, font=font)
     text_width = text_bbox[2] - text_bbox[0]
     text_height = text_bbox[3] - text_bbox[1]
-    
+
     label_x = x1 - text_width - 100  # 100 pixels gap for the arrow
     label_y = y_center - (text_height // 2)
-    
+
     # Draw label text in red
     draw.text((label_x, label_y), label, fill="red", font=font)
-    
+
     # Draw arrow from text to box
     arrow_start = (label_x + text_width + 10, y_center)
     arrow_end = (x1 - 10, y_center)
     draw.line([arrow_start, arrow_end], fill="red", width=3)
-    
+
     # Simple arrow head
     head_size = 10
     draw.polygon([
